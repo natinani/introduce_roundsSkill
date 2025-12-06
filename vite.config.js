79bc7feb43121219ad1999f418eb68eff5c53763
natinani <hirochikeitai@gmail.com>
2025-12-06 22:40:59 +0900

    rounds icon,base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/introduce_roundsSkill/",
+  base: "/introduce_roundsSkill/",
   plugins: [react()],
 })
