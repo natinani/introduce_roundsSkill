6145dad0cd6c8841cf2631ffc65d75be7d1eec94
natinani <hirochikeitai@gmail.com>
2025-12-06 22:37:12 +0900

    rounds icon -> github pages rounds icon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="container mt-4">
       <h1>ホーム</h1>
       <img 
-        src="/rounds_icon.png" 
+        src="/introduce_roundsSkill/rounds_icon.png" 
         alt="roundsIcon"
         style={{ width: "200px", height: "auto" }} 
       />
